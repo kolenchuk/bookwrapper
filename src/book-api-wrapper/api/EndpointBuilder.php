b81fdd001297042ad2943591256a215b75a7f754
kolenchuk <kolenchuk@gmail.com>
2019-06-17 10:45:16 +0300

    fix: return value for endpointbuilder

---
+++ src/book-api-wrapper/api/EndpointBuilder.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ */
+
+namespace BookApiWrapper\Api;
+
+
+class EndpointBuilder
+{
+
+    /**
+     * Get endpoint for authors request
+     *
+     * @param int $limit
+     * @param int $offset
+     * @return string
+     */
+    public static function getAuthors($limit = 0, $offset = 0)
+    {
+        $params = [];
+        if (!empty($limit)) {
+            $params[] = 'limit=' . $limit;
+        }
+
+        if (!empty($offset)) {
+            $params[] = 'offset=' . $offset;
+        }
+
+        return 'http://94.254.0.188:4000/authors' . ((!empty($params)) ? '?' . implode('&', $params) : '');
+    }
+}
